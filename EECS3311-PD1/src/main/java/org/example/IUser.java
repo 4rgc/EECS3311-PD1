1c985096aa7c2da3fbb854f4fe3a337b24f726cb
4rgc <bohdan.andrii@gmail.com>
2022-10-09 14:43:17 -0400

    feat: add IUser interface and User class

---
+++ EECS3311-PD1/src/main/java/org/example/IUser.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public interface IUser {
+    String getKey();
+
+    String getUsername();
+
+    void setUsername(String username);
+
+    String getPassword();
+
+    void setPassword(String newPassword);
+}
---
+++ EECS3311-PD1/src/main/java/org/example/User.java
@@ -0,0 +1,38 @@
+package org.example;
+
+public class User implements IUser {
+    private final String key;
+    private String username;
+    private String password;
+
+    public User(IRecord record) throws ClassCastException {
+        this.key = record.getKey();
+        this.username = (String) record.getCell("username");
+        this.username = (String) record.getCell("password");
+    }
+
+    @Override
+    public String getKey() {
+        return key;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.username;
+    }
+
+    @Override
+    public void setUsername(String newUsername) {
+        this.username = newUsername;
+    }
+
+    @Override
+    public String getPassword() {
+        return this.password;
+    }
+
+    @Override
+    public void setPassword(String newPassword) {
+        this.password = newPassword;
+    }
+}
