b3486913539c468d0b02c9827a1ea4c6af404a98
Abdullah <abdullahsajjad2001@gmail.com>
2022-10-09 13:38:21 -0400

    Create Request Class, Response Class, and WbApiModel Class

--- EECS3311-PD1/src/main/java/org/example/Main.java
+++ EECS3311-PD1/src/main/java/org/example/Main.java
@@ -1,7 +1,26 @@
 package org.example;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Bruh testing!");
+    public static void main(String[] args) throws Exception {
+
+        Request request = new Request("CAN", "SP.POP.TOTL"
+                , "2000", "2002");
+        WbApiModel model = new WbApiModel();
+
+        Response response = model.getData(request);
+
+        int value = response.getValue(2000);
+
+        System.out.println(value);
+
+
     }
 }
