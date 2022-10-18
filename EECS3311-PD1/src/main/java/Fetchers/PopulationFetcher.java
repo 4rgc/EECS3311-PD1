d3ee954fd5bcec81fe224b742f7214f6f4b35263
KediMengistu <kedim22@my.yorku.ca>
2022-10-18 16:42:14 -0400

    feat: Add PopulationFetcher Class and it's test.

---
+++ EECS3311-PD1/src/main/java/Fetchers/PopulationFetcher.java
@@ -0,0 +1,9 @@
+package Fetchers;
+
+public class PopulationFetcher extends AbstractFetcher{
+    public PopulationFetcher(String sYear, String eYear, String country){
+        super(sYear, eYear, country);
+        this.indicatorID = "SP.POP.TOTL";
+    }
+
+}
--- EECS3311-PD1/src/test/java/Fetchers/FetchersTest.java
+++ EECS3311-PD1/src/test/java/Fetchers/FetchersTest.java
@@ -1,10 +1,7 @@
 package Fetchers;
 
 import org.example.WbApiModel;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -22,113 +19,80 @@ public class FetchersTest {
     }
 
     @Test
-    void airPollutionTester() {
+    void airPollutionTester() throws WbApiModel.WbApiModelException {
         AirPollutionFetcher airPollution = new AirPollutionFetcher(startYear, endYear, country);
-        try {
-            Map<String, Double> map = new HashMap<String, Double>(airPollution.getData());
-            Assertions.assertEquals(map.get("2000"), 8.2809835638994);
-            Assertions.assertNull(map.get("2001"));
-        } catch (WbApiModel.WbApiModelException e) {
-            throw new RuntimeException(e);
-        }
+        Map<String, Double> map = new HashMap<String, Double>(airPollution.getData());
+        Assertions.assertEquals(map.get(startYear), 8.2809835638994);
+        Assertions.assertNull(map.get(endYear));
     }
     @Test
-    void co2Tester() {
+    void co2Tester() throws WbApiModel.WbApiModelException {
         CO2Fetcher co2 = new CO2Fetcher(startYear, endYear, country);
-        try {
-            Map<String, Double> map = new HashMap<String, Double>(co2.getData());
-            Assertions.assertEquals(map.get("2000"), 16.7576264276587);
-            Assertions.assertEquals(map.get("2001"), 16.3315689530885);
-        } catch (WbApiModel.WbApiModelException e) {
-            throw new RuntimeException(e);
-        }
+        Map<String, Double> map = new HashMap<String, Double>(co2.getData());
+        Assertions.assertEquals(map.get(startYear), 16.7576264276587);
+        Assertions.assertEquals(map.get(endYear), 16.3315689530885);
     }
     @Test
-    void educationExpenditureTester() {
+    void educationExpenditureTester() throws WbApiModel.WbApiModelException {
         EducationExpenditureFetcher educationExpindenture = new EducationExpenditureFetcher(startYear, endYear, country);
-        try {
-            Map<String, Double> map = new HashMap<String, Double>(educationExpindenture.getData());
-            Assertions.assertEquals(map.get("2000"), 12.9826002120972);
-            Assertions.assertEquals(map.get("2001"), 12.2122402191162);
-        } catch (WbApiModel.WbApiModelException e) {
-            throw new RuntimeException(e);
-        }
+        Map<String, Double> map = new HashMap<String, Double>(educationExpindenture.getData());
+        Assertions.assertEquals(map.get(startYear), 12.9826002120972);
+        Assertions.assertEquals(map.get(endYear), 12.2122402191162);
     }
     @Test
-    void energyUseTester() {
+    void energyUseTester() throws WbApiModel.WbApiModelException {
         EnergyUseFetcher energyUse = new EnergyUseFetcher(startYear, endYear, country);
-        try {
-            Map<String, Double> map = new HashMap<String, Double>(energyUse.getData());
-            Assertions.assertEquals(map.get("2000"), 8265.07992477285);
-            Assertions.assertEquals(map.get("2001"), 8056.3494253004);
-        } catch (WbApiModel.WbApiModelException e) {
-            throw new RuntimeException(e);
-        }
+        Map<String, Double> map = new HashMap<String, Double>(energyUse.getData());
+        Assertions.assertEquals(map.get(startYear), 8265.07992477285);
+        Assertions.assertEquals(map.get(endYear), 8056.3494253004);
     }
     @Test
-    void forestAreaTester() {
+    void forestAreaTester() throws WbApiModel.WbApiModelException {
         ForestAreaFetcher forestArea = new ForestAreaFetcher(startYear, endYear, country);
-        try {
-            Map<String, Double> map = new HashMap<String, Double>(forestArea.getData());
-            Assertions.assertEquals(map.get("2000"), 38.7929818338782);
-            Assertions.assertEquals(map.get("2001"), 38.7876307080739);
-        } catch (WbApiModel.WbApiModelException e) {
-            throw new RuntimeException(e);
-        }
+        Map<String, Double> map = new HashMap<String, Double>(forestArea.getData());
+        Assertions.assertEquals(map.get(startYear), 38.7929818338782);
+        Assertions.assertEquals(map.get(endYear), 38.7876307080739);
     }
     @Test
-    void gdpTester() {
+    void gdpTester() throws WbApiModel.WbApiModelException {
         GDPFetcher gdp = new GDPFetcher(startYear, endYear, country);
-        try {
-            Map<String, Double> map = new HashMap<String, Double>(gdp.getData());
-            Assertions.assertEquals(map.get("2000"), 24271.0020563821);
-            Assertions.assertEquals(map.get("2001"), 23822.0601178964);
-        } catch (WbApiModel.WbApiModelException e) {
-            throw new RuntimeException(e);
-        }
+        Map<String, Double> map = new HashMap<String, Double>(gdp.getData());
+        Assertions.assertEquals(map.get(startYear), 24271.0020563821);
+        Assertions.assertEquals(map.get(endYear), 23822.0601178964);
     }
     @Test
-    void healthExpenditureTester() {
+    void healthExpenditureTester() throws WbApiModel.WbApiModelException {
         HealthExpenditureFetcher healthExpenditure = new HealthExpenditureFetcher(startYear, endYear, country);
-        try {
-            Map<String, Double> map = new HashMap<String, Double>(healthExpenditure.getData());
-            Assertions.assertEquals(map.get("2000"), 8.24814415);
-            Assertions.assertEquals(map.get("2001"), 8.62482357);
-        } catch (WbApiModel.WbApiModelException e) {
-            throw new RuntimeException(e);
-        }
+        Map<String, Double> map = new HashMap<String, Double>(healthExpenditure.getData());
+        Assertions.assertEquals(map.get(startYear), 8.24814415);
+        Assertions.assertEquals(map.get(endYear), 8.62482357);
     }
     @Test
-    void hospitalBedsTester() {
+    void hospitalBedsTester() throws WbApiModel.WbApiModelException {
         HospitalBedsFetcher hospitalBeds = new HospitalBedsFetcher(startYear, endYear, country);
-        try {
-            Map<String, Double> map = new HashMap<String, Double>(hospitalBeds.getData());
-            Assertions.assertEquals(map.get("2000"), 3.77);
-            Assertions.assertEquals(map.get("2001"), 3.69);
-        } catch (WbApiModel.WbApiModelException e) {
-            throw new RuntimeException(e);
-        }
+        Map<String, Double> map = new HashMap<String, Double>(hospitalBeds.getData());
+        Assertions.assertEquals(map.get(startYear), 3.77);
+        Assertions.assertEquals(map.get(endYear), 3.69);
     }
     @Test
-    void mortalityRateTester() {
+    void mortalityRateTester() throws WbApiModel.WbApiModelException {
         MortalityRateFetcher mortalityRate = new MortalityRateFetcher(startYear, endYear, country);
-        try {
-            Map<String, Double> map = new HashMap<String, Double>(mortalityRate.getData());
-            Assertions.assertEquals(map.get("2000"), 9);
-            Assertions.assertEquals(map.get("2001"), 10);
-        } catch (WbApiModel.WbApiModelException e) {
-            throw new RuntimeException(e);
-        }
+        Map<String, Double> map = new HashMap<String, Double>(mortalityRate.getData());
+        Assertions.assertEquals(map.get(startYear), 9);
+        Assertions.assertEquals(map.get(endYear), 10);
     }
     @Test
-    void problemsIAHCTester() {
+    void populationTester() throws WbApiModel.WbApiModelException {
+        PopulationFetcher population = new PopulationFetcher(startYear, endYear, country);
+        Map<String, Double> map = new HashMap<String, Double>(population.getData());
+        Assertions.assertEquals(map.get(startYear), 30685730);
+        Assertions.assertEquals(map.get(endYear), 31020902);
+    }
+    @Test
+    void problemsIAHCTester() throws WbApiModel.WbApiModelException {
         ProblemsIAHCFetcher problemsIAHC = new ProblemsIAHCFetcher(startYear, endYear, country);
-        try {
-            Map<String, Double> map = new HashMap<String, Double>(problemsIAHC.getData());
-            Assertions.assertNull(map.get("2000"));
-            Assertions.assertNull(map.get("2001"));
-        } catch (WbApiModel.WbApiModelException e) {
-            throw new RuntimeException(e);
-        }
+        Map<String, Double> map = new HashMap<String, Double>(problemsIAHC.getData());
+        Assertions.assertNull(map.get(startYear));
+        Assertions.assertNull(map.get(endYear));
     }
 }
