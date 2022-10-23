56f00442b555da04a172b21ef13492786ea9a4d1
4rgc <bohdan.andrii@gmail.com>
2022-10-23 14:42:34 -0400

    fix: remove code from Main method

--- EECS3311-PD1/src/main/java/org/example/Main.java
+++ EECS3311-PD1/src/main/java/org/example/Main.java
@@ -1,28 +1,5 @@
 package org.example;
 
-
-import Analyzer.AbstractAnalyzer;
-import Analyzer.AirPollutionVsForestArea;
-import Analyzer.AnalysisResult;
-
-import java.util.Map;
-
 public class Main {
-    public static void main(String[] args) throws Exception {
-
-        AbstractAnalyzer analyzer = new AirPollutionVsForestArea("2000", "2000", "CAN");
-        AnalysisResult result = analyzer.recalculate();
-
-        String[] Label = result.getLabels();
-        Map<String, Double> Data = result.getValues(2000);
-
-        for(int i=0; i<Label.length; i++){
-            System.out.print(Label[i] + " ");
-        }
-        System.out.println();
-        System.out.print(Data);
-
-
-
-    }
+    public static void main(String[] args) {  }
 }
