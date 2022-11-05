413f33cc054242b683a52b07dfd14975a55fe62f
4rgc <bohdan.andrii@gmail.com>
2022-11-05 18:04:50 -0400

    feat: add an abstract UIComponent class

---
+++ EECS3311-PD1/src/main/java/uiComponents/UIComponent.java
@@ -0,0 +1,11 @@
+package uiComponents;
+
+import javax.swing.*;
+
+public abstract class UIComponent {
+    protected JComponent jComponent;
+
+    public JComponent getJComponent() {
+        return jComponent;
+    }
+}
