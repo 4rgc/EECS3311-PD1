7b938c974e35f91b9b8b0c1d0087053cdcc33383
4rgc <bohdan.andrii@gmail.com>
2022-11-07 14:09:47 -0500

    feat: use the login view in the app

--- EECS3311-PD1/src/main/java/UI/Main.java
+++ EECS3311-PD1/src/main/java/UI/Main.java
@@ -1,19 +1,26 @@
 package UI;
 
 import javafx.application.Application;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Objects;
 
 public class Main extends Application {
 
     @Override
-    public void start(Stage stage) {
-        String javaVersion = System.getProperty("java.version");
-        String javafxVersion = System.getProperty("javafx.version");
-        Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-        Scene scene = new Scene(new StackPane(l), 640, 480);
+    public void start(Stage stage) throws IOException {
+        Parent pane = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/fxml/LoginView.fxml")));
+
+        Scene scene = new Scene(pane);
         stage.setScene(scene);
         stage.show();
     }
