1a58d99a970efe9f3d3df62128f14e465e8354fe
Abdullah <abdullahsajjad2001@gmail.com>
2022-11-20 11:17:29 -0500

    Custom Control modification

--- EECS3311-PD1/src/main/java/UI/View/ReportView.java
+++ EECS3311-PD1/src/main/java/UI/View/ReportView.java
@@ -4,24 +4,39 @@ import Analyzer.AbstractAnalyzer;
 import Analyzer.AnalysisResult;
 import Analyzer.Co2VsEnergyUseVsAirPollution;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.control.TextArea;
+import javafx.scene.layout.VBox;
 import org.example.WbApiModel;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Map;
 
-public class ReportViewController {
+public class ReportView extends VBox {
     public AnalysisResult result;
     public ArrayList<Map<String, Double>> data;
     @FXML
     public TextArea textAreaReport;
     private static final String INDENT = "\t\t";
 
-    public ReportViewController(){}
-    public ReportViewController(AnalysisResult result){
+  //  public ReportViewController(){}
+    public ReportView(AnalysisResult result){
         this.result = result;
         this.data = result.getData();
+
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(
+                "/fxml/ReportView.fxml"));
+        fxmlLoader.setRoot(this);
+        fxmlLoader.setController(this);
+
+        try {
+            fxmlLoader.load();
+        } catch (IOException exception) {
+            throw new RuntimeException(exception);
+        }
     }
+
     public String generateReport(int startYear, int endYear, String AnalysisMethod){
         String[] Labels = result.getLabels();
         StringBuilder reportBuilder = new StringBuilder(AnalysisMethod + "\n");
@@ -46,7 +61,7 @@ public class ReportViewController {
         );
         AnalysisResult result = analyzer.recalculate(analyzer.getStartYear(), analyzer.getEndYear(), analyzer.getCountry());
 
-        ReportViewController rp = new ReportViewController(result);
+        ReportView rp = new ReportView(result);
         String a = rp.generateReport(2015, 2021, analyzer.getClass().getSimpleName());
         System.out.println(a);
     }
