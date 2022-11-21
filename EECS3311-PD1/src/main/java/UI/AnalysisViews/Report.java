510e820f8d604e1ca0030680092e8d1fa75cb5ac
4rgc <bohdan.andrii@gmail.com>
2022-11-21 13:26:00 -0500

    refactor: move the Report view

--- EECS3311-PD1/src/main/java/UI/AnalysisViews/Report.java
+++ EECS3311-PD1/src/main/java/UI/AnalysisViews/Report.java
@@ -1,31 +1,30 @@
-package UI.View;
+package UI.AnalysisViews;
 
-import Analyzer.AbstractAnalyzer;
 import Analyzer.AnalysisResult;
-import Analyzer.Co2VsEnergyUseVsAirPollution;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.TextArea;
 import javafx.scene.layout.VBox;
-import org.example.WbApiModel;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Map;
 
-public class ReportView extends VBox {
+public class Report extends VBox {
     public AnalysisResult result;
     public ArrayList<Map<String, Double>> data;
     @FXML
     public TextArea textAreaReport;
     private static final String INDENT = "\t\t";
 
-    public ReportView(AnalysisResult result){
+    public Report(AnalysisResult result){
         this.result = result;
         this.data = result.getData();
 
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(
-                "/fxml/ReportView.fxml"));
+                "/fxml/Report.fxml"));
         fxmlLoader.setRoot(this);
         fxmlLoader.setController(this);
 
@@ -36,12 +35,23 @@ public class ReportView extends VBox {
         }
     }
 
-    public void generateReport(int startYear, int endYear, String AnalysisMethod){
+    public void generateReport(String analysisMethod){
         String[] Labels = result.getLabels();
-        StringBuilder reportBuilder = new StringBuilder(AnalysisMethod + "\n");
+
+        Map<String, Double> yearToValue = data.get(0);
+        List<Integer> intYears = new ArrayList<>();
+        for (String year:
+                yearToValue.keySet()) {
+            intYears.add(Integer.parseInt(year));
+        }
+
+        Integer minYear = Collections.min(intYears),
+                maxYear = Collections.max(intYears);
+
+        StringBuilder reportBuilder = new StringBuilder(analysisMethod + "\n");
         reportBuilder.append("==========================\n");
 
-        for(int i=endYear; i>=startYear;i--){
+        for(int i=maxYear; i>=minYear;i--){
             reportBuilder.append("Year " + i + ":\n");
             for(int j=0; j<Labels.length;j++){
                 reportBuilder.append(INDENT+Labels[j] + " => " + this.data.get(j).get(Integer.toString(i)) + "\n");
