eef275be1852056d65798bfd847792e21df0212c
4rgc <bohdan.andrii@gmail.com>
2022-12-02 08:39:10 -0500

    ci: make LoginViewTest work in CI

--- EECS3311-PD1/src/test/java/UI/LoginViewTest.java
+++ EECS3311-PD1/src/test/java/UI/LoginViewTest.java
@@ -2,33 +2,33 @@ package UI;
 
 import javafx.application.Platform;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.input.KeyCode;
-import javafx.scene.input.MouseButton;
 import javafx.stage.Stage;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.loadui.testfx.GuiTest;
 import org.testfx.api.FxRobot;
-import org.testfx.api.FxToolkit;
 import org.testfx.framework.junit5.ApplicationExtension;
-import org.testfx.framework.junit5.ApplicationTest;
 import org.testfx.framework.junit5.Start;
 import org.testfx.matcher.control.LabeledMatchers;
 
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 import static org.testfx.api.FxAssert.verifyThat;
 
 @ExtendWith(ApplicationExtension.class)
 class LoginViewTest {
+    static {
+        if (Boolean.getBoolean("headless")) {
+            System.setProperty("testfx.robot", "glass");
+            System.setProperty("testfx.headless", "true");
+            System.setProperty("prism.order", "sw");
+            System.setProperty("prism.text", "t2k");
+        }
+    }
+
     private Stage stage;
 
     @Start
-    public void start(Stage stage) throws Exception {
+    public void start(Stage stage) {
         this.stage = stage;
         Platform.runLater(() -> {
             stage.setScene(new Scene(new LoginView()));
@@ -38,14 +38,12 @@ class LoginViewTest {
     }
 
     @BeforeEach
-    public void setUp() throws Exception {
+    public void setUp() {
     }
 
     @AfterEach
-    public void tearDown() throws Exception {
-        Platform.runLater(() -> {
-            stage.hide();
-        });
+    public void tearDown() {
+        Platform.runLater(() -> stage.hide());
     }
 
     @Test
